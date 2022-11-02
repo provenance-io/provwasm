d8124fee99150c22ae9a35f811514b91be29d42c
Ken Talley <kwtalley@users.noreply.github.com>
2022-11-02 12:43:05 -0500

    use `cw_schema` for msgfees contract message schema generation

--- contracts/msgfees/src/msg.rs
+++ contracts/msgfees/src/msg.rs
@@ -1,16 +1,13 @@
+use cosmwasm_schema::cw_serde;
 use cosmwasm_std::{Addr, Coin};
-use schemars::JsonSchema;
-use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Clone, Debug, Eq, PartialEq, JsonSchema)]
-#[serde(rename_all = "snake_case")]
+#[cw_serde]
 pub struct InitMsg {
     pub fee_amount: Option<Coin>,
     pub fee_recipient: Option<Addr>,
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug, Eq, PartialEq, JsonSchema)]
-#[serde(rename_all = "snake_case")]
+#[cw_serde]
 pub enum ExecuteMsg {
     SendFunds { funds: Coin, to_address: Addr },
 }
