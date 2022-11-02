83f2d56d72538c18d5a5d0716b34733f19defeae
Ken Talley <kwtalley@users.noreply.github.com>
2022-11-02 12:43:49 -0500

    use `cw_schema` for scope contract message schema generation

--- contracts/scope/examples/schema.rs
+++ contracts/scope/examples/schema.rs
@@ -1,23 +1,10 @@
-use std::env::current_dir;
-use std::fs::create_dir_all;
-
-use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
-use provwasm_std::{Records, Scope, Sessions};
-
+use cosmwasm_schema::write_api;
 use scope::msg::{ExecuteMsg, InitMsg, QueryMsg};
-use scope::state::State;
 
 fn main() {
-    let mut out_dir = current_dir().unwrap();
-    out_dir.push("schema");
-    create_dir_all(&out_dir).unwrap();
-    remove_schemas(&out_dir).unwrap();
-
-    export_schema(&schema_for!(InitMsg), &out_dir);
-    export_schema(&schema_for!(ExecuteMsg), &out_dir);
-    export_schema(&schema_for!(QueryMsg), &out_dir);
-    export_schema(&schema_for!(Records), &out_dir);
-    export_schema(&schema_for!(Scope), &out_dir);
-    export_schema(&schema_for!(Sessions), &out_dir);
-    export_schema(&schema_for!(State), &out_dir);
+    write_api! {
+        execute: ExecuteMsg,
+        instantiate: InitMsg,
+        query: QueryMsg,
+    }
 }
--- contracts/scope/src/msg.rs
+++ contracts/scope/src/msg.rs
@@ -1,25 +1,26 @@
+use cosmwasm_schema::{cw_serde, QueryResponses};
 use cosmwasm_std::Addr;
 use provwasm_std::Scope;
-use schemars::JsonSchema;
-use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Clone, Debug, Eq, PartialEq, JsonSchema)]
-#[serde(rename_all = "snake_case")]
+#[cw_serde]
 pub struct InitMsg {
     pub name: String, // Bind this name to the contract address (eg contract.pb).
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug, Eq, PartialEq, JsonSchema)]
-#[serde(rename_all = "snake_case")]
+#[cw_serde]
 pub enum ExecuteMsg {
     WriteScope { scope: Scope, signers: Vec<Addr> },
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug, Eq, PartialEq, JsonSchema)]
-#[serde(rename_all = "snake_case")]
+#[cw_serde]
+#[derive(QueryResponses)]
 pub enum QueryMsg {
+    #[returns(provwasm_std::Scope)]
     GetScope { id: String },
+    #[returns(provwasm_std::Sessions)]
     GetSessions { scope_id: String },
+    #[returns(provwasm_std::Records)]
     GetRecords { scope_id: String },
+    #[returns(provwasm_std::Record)]
     GetRecordByName { scope_id: String, name: String },
 }
