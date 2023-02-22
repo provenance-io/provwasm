5040489b5b4f1448500c656719ba44d43913d539
Ken Talley <kwtalley@users.noreply.github.com>
2023-02-22 01:33:45 -0600

    temp remove generated types

--- packages/provwasm-std/src/types/cosmos/mod.rs
+++
@@ -1,4 +0,0 @@
-pub mod auth;
-pub mod bank;
-pub mod base;
-pub mod upgrade;
--- packages/provwasm-std/src/types/provenance/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod attribute;
-pub mod marker;
-pub mod metadata;
-pub mod msgfees;
-pub mod name;
-pub mod reward;
