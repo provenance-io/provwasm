3ca03974950a4f46ea0adbcde40e65742b727951
Matt Witkowski <taztingo503@gmail.com>
2023-04-11 01:29:13 -0400

    Simplified change_owner by removing one state retrieval.

--- contracts/template/src/execute/change_owner.rs
+++ contracts/template/src/execute/change_owner.rs
@@ -15,10 +15,9 @@ pub fn handle(deps: ProvDepsMut, sender: Addr, new_owner: Addr) -> ProvTxRespons
         return Err(ContractError::Unauthorized {});
     }
 
-    let previous_owner = storage::state::get_owner(deps.storage)?;
     storage::state::set_owner(deps.storage, new_owner.clone())?;
 
     Ok(Response::default()
         .set_action(ActionType::ChangeOwner {})
-        .add_event(ChangeOwnerEvent::new(previous_owner, new_owner).into()))
+        .add_event(ChangeOwnerEvent::new(sender, new_owner).into()))
 }
