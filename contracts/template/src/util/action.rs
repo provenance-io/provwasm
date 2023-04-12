3f4bd90cdf96f3ff36097c74a8465151e08ab3c1
Matt Witkowski <taztingo503@gmail.com>
2023-04-11 23:53:30 -0400

    Added tests for util.

--- contracts/template/src/util/action.rs
+++ contracts/template/src/util/action.rs
@@ -38,3 +38,45 @@ impl Action for ProvResponse {
         self.add_attribute(ACTION_ATTRIBUTE, action_type.to_string())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use cosmwasm_std::Attribute;
+
+    use crate::core::constants::{
+        ACTION_ATTRIBUTE, ACTION_TYPE_CHANGE_OWNER, ACTION_TYPE_INITIALIZE, ACTION_TYPE_MIGRATE,
+    };
+
+    use super::ActionType;
+
+    #[test]
+    fn test_to_string() {
+        let action_type1 = ActionType::ChangeOwner;
+        let action_type2 = ActionType::Initialize;
+        let action_type3 = ActionType::Migrate;
+        assert_eq!(
+            ACTION_TYPE_CHANGE_OWNER.to_string(),
+            action_type1.to_string()
+        );
+        assert_eq!(ACTION_TYPE_INITIALIZE.to_string(), action_type2.to_string());
+        assert_eq!(ACTION_TYPE_MIGRATE.to_string(), action_type3.to_string());
+    }
+
+    #[test]
+    fn test_from() {
+        let action_type1 = ActionType::ChangeOwner;
+        let action_type2 = ActionType::Initialize;
+        let action_type3 = ActionType::Migrate;
+
+        let attribute1 = Attribute::from(action_type1);
+        let attribute2 = Attribute::from(action_type2);
+        let attribute3 = Attribute::from(action_type3);
+
+        assert_eq!(attribute1.key, ACTION_ATTRIBUTE);
+        assert_eq!(attribute2.key, ACTION_ATTRIBUTE);
+        assert_eq!(attribute3.key, ACTION_ATTRIBUTE);
+        assert_eq!(attribute1.value, ACTION_TYPE_CHANGE_OWNER);
+        assert_eq!(attribute2.value, ACTION_TYPE_INITIALIZE);
+        assert_eq!(attribute3.value, ACTION_TYPE_MIGRATE);
+    }
+}
--- contracts/template/src/util/state.rs
+++ contracts/template/src/util/state.rs
@@ -14,3 +14,27 @@ impl State {
         State { owner, fee }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use cosmwasm_std::{Addr, Coin};
+
+    use crate::{
+        testing::constants::{OWNER, TEST_DENOM},
+        util::fee::Fee,
+    };
+
+    use super::State;
+
+    #[test]
+    fn test_new() {
+        let owner = Addr::unchecked(OWNER);
+        let fee = Fee {
+            recipient: None,
+            amount: Coin::new(100, TEST_DENOM),
+        };
+        let state = State::new(Addr::unchecked(OWNER), fee.clone());
+        assert_eq!(owner, state.owner);
+        assert_eq!(fee, state.fee);
+    }
+}
