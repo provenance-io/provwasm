315b847de0377d6b59a1ec3716d229f52912233d
Matt Witkowski <taztingo503@gmail.com>
2023-04-12 00:00:23 -0400

    Added tests for events.

--- contracts/template/src/events/change_owner.rs
+++ contracts/template/src/events/change_owner.rs
@@ -25,3 +25,38 @@ impl From<ChangeOwnerEvent> for Event {
         ])
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use cosmwasm_std::{Addr, Attribute, Event};
+
+    use crate::core::constants::{CHANGE_OWNER_EVENT, CHANGE_OWNER_NEW, CHANGE_OWNER_PREVIOUS};
+
+    use super::ChangeOwnerEvent;
+
+    #[test]
+    fn test_new_change_owner_event() {
+        let previous_owner = Addr::unchecked("previous");
+        let new_owner = Addr::unchecked("new");
+        let event = ChangeOwnerEvent::new(previous_owner.clone(), new_owner.clone());
+        assert_eq!(previous_owner, event.previous_owner);
+        assert_eq!(new_owner, event.new_owner);
+    }
+
+    #[test]
+    fn test_from() {
+        let previous_owner = Addr::unchecked("previous");
+        let new_owner = Addr::unchecked("new");
+        let event = ChangeOwnerEvent::new(previous_owner.clone(), new_owner.clone());
+
+        let mut expected = Event::new(CHANGE_OWNER_EVENT);
+        expected = expected.add_attributes([
+            Attribute::new(CHANGE_OWNER_PREVIOUS, previous_owner.to_string()),
+            Attribute::new(CHANGE_OWNER_NEW, new_owner.to_string()),
+        ]);
+
+        let from_event = Event::from(event);
+
+        assert_eq!(expected, from_event);
+    }
+}
