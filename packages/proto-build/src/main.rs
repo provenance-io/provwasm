43a7f2999adc1afe249773f4dfd3a4ca8c359a0d
kwt <4344285+kwtalley@users.noreply.github.com>
2023-04-17 16:10:06 -0500

    update to provenance v1.15.0-rc1

--- packages/proto-build/src/main.rs
+++ packages/proto-build/src/main.rs
@@ -10,7 +10,7 @@ use proto_build::{
 };
 
 /// The provenance commit or tag to be cloned and used to build the proto files
-const PROVENANCE_REV: &str = "v1.14.1";
+const PROVENANCE_REV: &str = "v1.15.0-rc1";
 
 // All paths must end with a / and either be absolute or include a ./ to reference the current
 // working directory.
