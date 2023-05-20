de52c73236589fac08d87265a9010dc9286b6b0c
kwt <4344285+kwtalley@users.noreply.github.com>
2023-05-20 00:44:50 -0500

    fix scope contract test script

--- scripts/gh-action-test/scope_test.sh
+++ scripts/gh-action-test/scope_test.sh
@@ -46,4 +46,81 @@ export node0=$("$PROV_CMD" keys show -a validator --keyring-backend test --testn
   --yes \
   --testnet
 
+export contract=$("$PROV_CMD" query wasm list-contract-by-code 1 --testnet --output json  | jq -r ".contracts[0]")
+
+$PROV_CMD tx metadata write-contract-specification \
+  contractspec1q0w6ys5g6jm509v2830374aprsrq260w62 \
+  "$node0" \
+  "owner" \
+  "hashvalue" \
+  "myclassname" \
+  --from="$node0" \
+  --keyring-backend test \
+  --chain-id="testing" \
+  --gas=auto \
+  --gas-prices="1905nhash" \
+  --gas-adjustment=1.5 \
+  --broadcast-mode block \
+  --yes \
+  --testnet
+
+$PROV_CMD tx metadata write-scope-specification \
+  scopespec1qn7jh3jvw4gytq9r5x770e8yj74s9t479r \
+  "$node0" \
+  "OWNER" \
+  contractspec1q0w6ys5g6jm509v2830374aprsrq260w62 \
+  --from="$node0" \
+  --keyring-backend test \
+  --chain-id="testing" \
+  --gas=auto \
+  --gas-prices="1905nhash" \
+  --gas-adjustment=1.5 \
+  --broadcast-mode block \
+  --yes \
+  --testnet
+
+$PROV_CMD tx metadata write-scope \
+  scope1qzhpuff00wpy2yuf7xr0rp8aucqstsk0cn \
+  scopespec1qn7jh3jvw4gytq9r5x770e8yj74s9t479r \
+  "$node0" \
+  "$node0" \
+  "$node0" \
+  --from="$node0" \
+  --keyring-backend test \
+  --chain-id="testing" \
+  --gas=auto \
+  --gas-prices="1905nhash" \
+  --gas-adjustment=1.5 \
+  --broadcast-mode block \
+  --yes \
+  --testnet
+
+#"$PROV_CMD" tx wasm execute \
+#  "$contract" \
+#  '{
+#    "write_scope":{
+#      "scope":{
+#        "scope_id":"scopeid",
+#        "specification_id":"scopeid",
+#        "owners":['"$node0"'],
+#        "data_access":"access",
+#        "value_owner_address":"",
+#        "require_party_rollup":false,
+#      },
+#      "signers": ["one"],
+#    }
+#  }' \
+#  --from="$node0" \
+#  --keyring-backend test \
+#  --chain-id="testing" \
+#  --gas=auto \
+#  --gas-prices="1905nhash" \
+#  --gas-adjustment=1.5 \
+#  --broadcast-mode block \
+#  --yes \
+#  --testnet
+
+"$PROV_CMD" q wasm contract-state smart "$contract" '{"get_contract_spec":{"id":"contractspec1q0w6ys5g6jm509v2830374aprsrq260w62"}}' --testnet
+"$PROV_CMD" q wasm contract-state smart "$contract" '{"get_scope":{"id":"scope1qzhpuff00wpy2yuf7xr0rp8aucqstsk0cn"}}' --testnet
+
 echo "done with the scope contract"
