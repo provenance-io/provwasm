fd3e248082a257f8882e7df4e2e266d12c953d7a
kwt <4344285+kwtalley@users.noreply.github.com>
2023-05-20 23:41:48 -0500

    add new mocking querier

--- packages/provwasm-mocks/src/querier.rs
+++ packages/provwasm-mocks/src/querier.rs
@@ -1,10 +1,13 @@
 use crate::{AttributeQuerier, MarkerQuerier, MetadataQuerier, NameQuerier};
 use cosmwasm_std::testing::{MockApi, MockQuerier, MockStorage, MOCK_CONTRACT_ADDR};
 use cosmwasm_std::{
-    from_slice, to_binary, Coin, OwnedDeps, Querier, QuerierResult, QueryRequest, StdResult,
-    SystemError, SystemResult,
+    from_slice, to_binary, Binary, Coin, Empty, OwnedDeps, Querier, QuerierResult, QueryRequest,
+    StdResult, SystemError, SystemResult,
 };
+use provwasm_common::MockableQuerier;
 use provwasm_std::{Marker, ProvenanceQuery, ProvenanceQueryParams, Records, Scope, Sessions};
+use serde::de::DeserializeOwned;
+use std::collections::HashMap;
 use std::marker::PhantomData;
 
 /// A drop-in replacement for cosmwasm_std::testing::mock_dependencies that uses the mock
@@ -34,7 +37,7 @@ pub fn mock_dependencies_with_balances(
         custom_query_type: PhantomData,
     }
 }
-
+#[deprecated]
 pub struct ProvenanceMockQuerier {
     pub base: MockQuerier,
     name: NameQuerier,
@@ -116,6 +119,88 @@ impl ProvenanceMockQuerier {
     }
 }
 
+pub struct MockProvenanceQuerier<C: DeserializeOwned = Empty> {
+    /// Default CosmWASM mock querier.
+    pub mock_querier: MockQuerier<C>,
+    /// Registered custom queries using proto request for testing.
+    pub registered_custom_queries: HashMap<String, Box<dyn Fn(&Binary) -> QuerierResult>>,
+}
+
+impl MockProvenanceQuerier {
+    /// Initialize a new [`MockProvenanceQuerier`].
+    /// * `balances` - Slice of balances passed to the `bank` module, where the first element
+    /// is the user address and the second element is the user's balance.
+    pub fn new(balances: &[(&str, &[Coin])]) -> Self {
+        MockProvenanceQuerier {
+            mock_querier: MockQuerier::new(balances),
+            registered_custom_queries: HashMap::new(),
+        }
+    }
+    /// Handle the query request.
+    pub fn handle_query(&self, request: &QueryRequest<Empty>) -> QuerierResult {
+        match request {
+            QueryRequest::Stargate { path, data } => {
+                if let Some(response_fn) = self.registered_custom_queries.get(path) {
+                    return response_fn(data);
+                }
+                SystemResult::Err(SystemError::UnsupportedRequest { kind: path.into() })
+            }
+            _ => self.mock_querier.handle_query(request),
+        }
+    }
+}
+
+impl MockableQuerier for MockProvenanceQuerier {
+    fn register_custom_query(
+        &mut self,
+        path: String,
+        response_fn: Box<dyn Fn(&Binary) -> QuerierResult>,
+    ) {
+        self.registered_custom_queries.insert(path, response_fn);
+    }
+}
+
+impl Querier for MockProvenanceQuerier {
+    fn raw_query(&self, bin_request: &[u8]) -> QuerierResult {
+        let request: QueryRequest<Empty> = match from_slice(bin_request) {
+            Ok(v) => v,
+            Err(e) => {
+                return SystemResult::Err(SystemError::InvalidRequest {
+                    error: format!("Parsing query request: {}", e),
+                    request: bin_request.into(),
+                })
+            }
+        };
+        self.handle_query(&request)
+    }
+}
+
+impl Default for MockProvenanceQuerier {
+    fn default() -> Self {
+        MockProvenanceQuerier::new(&[])
+    }
+}
+
+/// Creates an instance of [`OwnedDeps`](cosmwasm_std::OwnedDeps) with a custom [`MockProvenanceQuerier`]
+/// to allow the user to mock the query responses of one or more Provenance's modules.
+pub fn mock_provenance_dependencies_with_custom_querier(
+    querier: MockProvenanceQuerier,
+) -> OwnedDeps<MockStorage, MockApi, MockProvenanceQuerier, Empty> {
+    OwnedDeps::<_, _, _, Empty> {
+        storage: MockStorage::default(),
+        api: MockApi::default(),
+        querier,
+        custom_query_type: PhantomData,
+    }
+}
+
+/// Creates an instance of [`OwnedDeps`](cosmwasm_std::OwnedDeps) that is capable of
+/// handling queries towards Provenance's modules.
+pub fn mock_provenance_dependencies(
+) -> OwnedDeps<MockStorage, MockApi, MockProvenanceQuerier, Empty> {
+    mock_provenance_dependencies_with_custom_querier(MockProvenanceQuerier::default())
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -125,6 +210,27 @@ mod test {
     fn query_with_balances() {
         let amount = coin(12345, "denom");
         let deps = mock_dependencies_with_balances(&[("alice", &[amount.clone()])]);
+        let bin = deps
+            .querier
+            .handle_query(
+                &BankQuery::Balance {
+                    address: "alice".into(),
+                    denom: "denom".into(),
+                }
+                .into(),
+            )
+            .unwrap()
+            .unwrap();
+
+        let res: BalanceResponse = from_binary(&bin).unwrap();
+        assert_eq!(res.amount, amount);
+    }
+
+    #[test]
+    fn query_with_balances_2_0() {
+        let amount = coin(12345, "denom");
+        let querier = MockProvenanceQuerier::new(&[("alice", &[amount.clone()])]);
+        let deps = mock_provenance_dependencies_with_custom_querier(querier);
         let bin = deps
             .querier
             .handle_query(
