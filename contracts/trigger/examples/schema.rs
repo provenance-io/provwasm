bdd7294d8275c992dcd4aa7c735520d3615a36c5
kwt <4344285+kwtalley@users.noreply.github.com>
2023-07-28 01:26:33 -0500

    add tests and remove queries. `Any` needs fairly large refactor to support query

--- contracts/trigger/examples/schema.rs
+++ contracts/trigger/examples/schema.rs
@@ -1,10 +1,9 @@
 use cosmwasm_schema::write_api;
-use trigger::msg::{ExecuteMsg, InitMsg, QueryMsg};
+use trigger::msg::{ExecuteMsg, InitMsg};
 
 fn main() {
     write_api! {
         execute: ExecuteMsg,
         instantiate: InitMsg,
-        query: QueryMsg,
     }
 }
--- contracts/trigger/src/contract.rs
+++ contracts/trigger/src/contract.rs
@@ -9,7 +9,7 @@ use provwasm_std::types::provenance::trigger::v1::{
 };
 
 use crate::error::ContractError;
-use crate::msg::{Event, ExecuteMsg, InitMsg, QueryMsg};
+use crate::msg::{Event, ExecuteMsg, InitMsg};
 
 /// Initialize the smart contract
 #[entry_point]
@@ -101,20 +101,130 @@ pub fn delete_trigger(
         .add_attribute("action", "provwasm.contracts.trigger.delete_trigger"))
 }
 
-/// Handle query requests for the provenance trigger module
-#[entry_point]
-pub fn query(deps: Deps, _env: Env, msg: QueryMsg) -> Result<QueryResponse, ContractError> {
-    match msg {
-        QueryMsg::GetById { id } => get_trigger(deps, Some(id)),
-        QueryMsg::GetAll {} => get_trigger(deps, None),
-    }
-}
-
 pub fn get_trigger(deps: Deps, id: Option<Uint64>) -> Result<QueryResponse, ContractError> {
     let trigger_querier = TriggerQuerier::new(&deps.querier);
 
     match id {
         Some(id) => Ok(to_binary(&trigger_querier.trigger_by_id(id.u64())?)?),
         None => Ok(to_binary(&trigger_querier.triggers(None)?)?),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::contract::{execute, instantiate};
+    use crate::msg::ExecuteMsg::CreateTrigger;
+    use crate::msg::{Event, InitMsg};
+    use cosmwasm_std::testing::{mock_env, mock_info};
+    use cosmwasm_std::{coin, Addr, Binary, CosmosMsg};
+    use provwasm_mocks::mock_provenance_dependencies;
+    use provwasm_std::shim::Timestamp;
+    use provwasm_std::types::cosmos::bank::v1beta1::MsgSend;
+    use provwasm_std::types::provenance::trigger::v1::{
+        BlockHeightEvent, BlockTimeEvent, MsgCreateTriggerRequest,
+    };
+
+    #[test]
+    pub fn init() {
+        let mut deps = mock_provenance_dependencies();
+
+        assert!(instantiate(
+            deps.as_mut(),
+            mock_env(),
+            mock_info("sender", &[]),
+            InitMsg {},
+        )
+        .is_ok());
+    }
+
+    #[test]
+    pub fn create_block_height() {
+        let mut deps = mock_provenance_dependencies();
+        let env = mock_env();
+        let info = mock_info("sender", &[coin(100, "hash")]);
+        let contract_address = env.contract.address.to_string();
+        let receiver = Addr::unchecked("receiver");
+
+        let expected_msg: Binary = MsgCreateTriggerRequest {
+            authorities: vec![contract_address],
+            event: Some(BlockHeightEvent { block_height: 50 }.try_into().unwrap()),
+            actions: vec![MsgSend {
+                from_address: env.contract.address.to_string(),
+                to_address: receiver.to_string(),
+                amount: vec![coin(100, "hash").into()],
+            }
+            .try_into()
+            .unwrap()],
+        }
+        .into();
+
+        let msg = CreateTrigger {
+            event: Event::BlockHeightEvent {
+                block_height: 50u64.into(),
+            },
+            to_address: receiver.to_string(),
+        };
+
+        let res = execute(deps.as_mut(), env, info, msg).unwrap();
+
+        assert_eq!(1, res.messages.len());
+
+        match &res.messages[0].msg {
+            CosmosMsg::Stargate { type_url, value } => {
+                assert_eq!(type_url, "/provenance.trigger.v1.MsgCreateTriggerRequest");
+                assert_eq!(value, &expected_msg)
+            }
+            _ => panic!("unexpected cosmos message"),
+        }
+    }
+
+    #[test]
+    pub fn create_block_time() {
+        let mut deps = mock_provenance_dependencies();
+        let env = mock_env();
+        let info = mock_info("sender", &[coin(100, "hash")]);
+        let contract_address = env.contract.address.to_string();
+        let receiver = Addr::unchecked("receiver");
+
+        let expected_msg: Binary = MsgCreateTriggerRequest {
+            authorities: vec![contract_address],
+            event: Some(
+                BlockTimeEvent {
+                    time: Some(Timestamp {
+                        seconds: 1690517980,
+                        nanos: 0,
+                    }),
+                }
+                .try_into()
+                .unwrap(),
+            ),
+            actions: vec![MsgSend {
+                from_address: env.contract.address.to_string(),
+                to_address: receiver.to_string(),
+                amount: vec![coin(100, "hash").into()],
+            }
+            .try_into()
+            .unwrap()],
+        }
+        .into();
+
+        let msg = CreateTrigger {
+            event: Event::BlockTimeEvent {
+                timestamp: 1690517980u64.into(),
+            },
+            to_address: receiver.to_string(),
+        };
+
+        let res = execute(deps.as_mut(), env, info, msg).unwrap();
+
+        assert_eq!(1, res.messages.len());
+
+        match &res.messages[0].msg {
+            CosmosMsg::Stargate { type_url, value } => {
+                assert_eq!(type_url, "/provenance.trigger.v1.MsgCreateTriggerRequest");
+                assert_eq!(value, &expected_msg)
+            }
+            _ => panic!("unexpected cosmos message"),
+        }
     }
 }
