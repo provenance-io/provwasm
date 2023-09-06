c987bfa5dfb2295d3886824db4e20ed699723262
kwt <4344285+kwtalley@users.noreply.github.com>
2023-09-05 22:26:18 -0500

    add approval error types

--- contracts/nft/src/core/error.rs
+++ contracts/nft/src/core/error.rs
@@ -1,4 +1,5 @@
 use cosmwasm_std::StdError;
+use cw_ownable::OwnershipError;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
@@ -6,6 +7,9 @@ pub enum ContractError {
     // General Errors
     #[error("{0}")]
     Std(#[from] StdError),
+
+    #[error("Cannot set approval that is already expired")]
+    Expired {},
 
     #[error("Token already exists with id: {id}")]
     TokenExists { id: String },
@@ -15,6 +19,9 @@ pub enum ContractError {
 
     #[error("Funds missing")]
     FundsMissing {},
+
+    #[error(transparent)]
+    Ownership(#[from] OwnershipError),
 
     #[error("Unauthorized")]
     Unauthorized {},
