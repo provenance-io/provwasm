624cabce70e06de72ef442a86682529abfc9a0dc
kwt <4344285+kwtalley@users.noreply.github.com>
2023-09-12 11:59:46 -0600

    add query::contract_info

--- contracts/nft/src/query/contract_info.rs
+++ contracts/nft/src/query/contract_info.rs
@@ -1,21 +1,8 @@
-use cosmwasm_std::{to_binary, Deps};
+use cosmwasm_std::{to_binary, Binary, Deps};
 
-use crate::core::aliases::Result<Binary, ContractError>;
-use crate::storage;
+use crate::core::error::ContractError;
+use crate::storage::contract_info::CONTRACT_INFO;
 
-/// Performs the logic for the QueryOwner message and obtains the contract's owner.
-///
-/// # Arguments
-///
-/// * `deps` - A non mutable version of the dependencies. The API, Querier, and storage can all be accessed from it.
-///
-/// # Examples
-/// ```
-/// let res = handle(deps)?;
-/// ```
 pub fn handle(deps: Deps) -> Result<Binary, ContractError> {
-    let res = QueryOwnerResponse {
-        owner: storage::state::get_owner(deps.storage)?,
-    };
-    Ok(to_binary(&res)?)
+    Ok(to_binary(&CONTRACT_INFO.load(deps.storage)?)?)
 }
