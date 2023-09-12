3605af6c83d3298a2fe29b8fd2bb063ede75d977
kwt <4344285+kwtalley@users.noreply.github.com>
2023-09-11 18:15:58 -0600

    add new constants for storage and queries

--- contracts/nft/src/core/constants.rs
+++ contracts/nft/src/core/constants.rs
@@ -3,11 +3,12 @@ pub const CONTRACT_NAME: &str = env!("CARGO_PKG_NAME");
 pub const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
 
 // Storage
-pub const STATE_KEY: &str = "state_key";
-pub const NFT_KEY: &str = "nft_key";
-pub const NFT_COUNT_KEY: &str = "nft_count_key";
-pub const NFT_OWNER_KEY: &str = "nft_owner_key";
-pub const OPERATORS_KEY: &str = "operators_key";
+pub const CONTRACT_INFO_KEY: &str = "contract_info";
+pub const STATE_KEY: &str = "state";
+pub const NFT_KEY: &str = "nft";
+pub const NFT_COUNT_KEY: &str = "nft_count";
+pub const NFT_OWNER_KEY: &str = "nft_owner";
+pub const OPERATORS_KEY: &str = "operators";
 
 // Actions
 pub const ACTION_ATTRIBUTE: &str = "action";
@@ -21,5 +22,9 @@ pub const ACTION_TYPE_REVOKE_ALL: &str = "revoke_all";
 pub const ACTION_TYPE_SEND: &str = "send";
 pub const ACTION_TYPE_TRANSFER: &str = "transfer";
 pub const ACTION_TYPE_MIGRATE: &str = "migrate";
+
+// Query
+pub const DEFAULT_LIMIT: u32 = 10;
+pub const MAX_LIMIT: u32 = 100;
 
 // Events
