28e3ccbd1decb1b15e3895a560e274dda23e2c14
kwt <4344285+kwtalley@users.noreply.github.com>
2023-09-12 12:40:10 -0600

    add query::contract_version

---
+++ contracts/nft/src/query/contract_version.rs
@@ -0,0 +1,18 @@
+use crate::core::error::ContractError;
+use cosmwasm_std::{to_binary, Binary, Deps};
+use cw2::get_contract_version;
+
+use crate::core::msg::ContractVersionResponse;
+
+/// Performs the logic for querying the contract version
+///
+/// # Arguments
+///
+/// * `deps` -  dependencies from the contract entrypoint
+///
+pub fn handle(deps: Deps) -> Result<Binary, ContractError> {
+    let res = ContractVersionResponse {
+        contract_version: get_contract_version(deps.storage)?,
+    };
+    Ok(to_binary(&res)?)
+}
