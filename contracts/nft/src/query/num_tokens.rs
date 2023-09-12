fab807e625deb541b189086f989615e526206df1
kwt <4344285+kwtalley@users.noreply.github.com>
2023-09-12 14:38:53 -0600

    add query::num_tokens

--- contracts/nft/src/query/num_tokens.rs
+++ contracts/nft/src/query/num_tokens.rs
@@ -1,10 +1,10 @@
-use crate::core::error::ContractError;
 use cosmwasm_std::{to_binary, Binary, Deps};
 use cw721::NumTokensResponse;
 
-use crate::storage::nft_count::NFT_COUNT;
+use crate::core::error::ContractError;
+use crate::storage::nft_count::nft_count;
 
 pub fn handle(deps: Deps) -> Result<Binary, ContractError> {
-    let count = NFT_COUNT.may_load(deps.storage)?.unwrap_or_default();
+    let count = nft_count(deps.storage)?;
     Ok(to_binary(&NumTokensResponse { count })?)
 }
