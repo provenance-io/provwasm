aa9a6def203a1dfa515c69c999fe1728c4d1b912
kwt <4344285+kwtalley@users.noreply.github.com>
2023-09-27 16:28:41 -0500

    add missing comments for messages

--- contracts/nft/src/core/msg.rs
+++ contracts/nft/src/core/msg.rs
@@ -28,16 +28,20 @@ pub enum ExecuteMsg {
     /// Allow operator to transfer / send the token from the owner's account.
     /// If expiration is set, then this allowance has a time/height limit
     Approve {
+        /// The operator to be allowed transfer / send of the token
         spender: Addr,
         /// Token ID in UUID format (Scope UUID)
         token_id: String,
+        /// An optional expiration of the approval
         expires: Option<Expiration>,
     },
 
     /// Allows operator to transfer / send any token from the owner's account.
     /// If expiration is set, then this allowance has a time/height limit
     ApproveAll {
+        /// The operator to be allowed transfer / send of the token
         operator: Addr,
+        /// An optional expiration of the approval
         expires: Option<Expiration>,
     },
 
@@ -59,13 +63,17 @@ pub enum ExecuteMsg {
 
     /// Remove previously granted Approval
     Revoke {
+        /// The operator to be removed from previous granted permission to transfer / send the token
         spender: Addr,
         /// Token ID in UUID format (Scope UUID)
         token_id: String,
     },
 
     /// Remove previously granted ApproveAll permission
-    RevokeAll { operator: Addr },
+    RevokeAll {
+        /// The operator to be removed from previous granted permission to transfer / send the token
+        operator: Addr,
+    },
 
     /// Transfer a token to a contract and trigger an action on the receiving contract.
     SendNft {
@@ -83,6 +91,7 @@ pub enum ExecuteMsg {
     TransferNft {
         /// Generated `UUID` to use for creating the `NFT`/`Scope`
         token_id: String,
+        /// Recipient of the NFT
         recipient: Addr,
         /// Generated `UUID` to use for creating the `Session` to write the new owner record
         session_uuid: String,
@@ -96,8 +105,11 @@ pub enum QueryMsg {
     /// Returns the operator approval details for a given token
     #[returns(cw721::ApprovalResponse)]
     Approval {
+        /// Token ID in UUID format (Scope UUID)
         token_id: String,
+        /// The operator allowed transfer / send of the token
         spender: String,
+        /// unset or false will filter out expired approvals, you must set to true to see them
         include_expired: Option<bool>,
     },
     /// Returns all operator approvals details for a given token
@@ -105,6 +117,7 @@ pub enum QueryMsg {
     Approvals {
         /// Token ID in UUID format (Scope UUID)
         token_id: String,
+        /// unset or false will filter out expired approvals, you must set to true to see them
         include_expired: Option<bool>,
     },
 
@@ -134,16 +147,23 @@ pub enum QueryMsg {
     /// Returns the operator approval details for all tokens of a given owner or an error if not set
     #[returns(cw721::OperatorResponse)]
     Operator {
+        /// Owner of the NFT
         owner: String,
+        /// The operator allowed transfer / send of the token
         operator: String,
+        /// unset or false will filter out expired approvals, you must set to true to see them
         include_expired: Option<bool>,
     },
     /// Returns all operator approvals details for all tokens of a given owner
     #[returns(cw721::OperatorsResponse)]
     AllOperators {
+        /// Owner of the NFT
         owner: String,
+        /// unset or false will filter out expired approvals, you must set to true to see them
         include_expired: Option<bool>,
+        /// The operator id to index from
         start_after: Option<String>,
+        /// The total number of operator details to return
         limit: Option<u32>,
     },
     /// Total number of tokens issued
@@ -170,15 +190,20 @@ pub enum QueryMsg {
     /// Returns all tokens owned by the given address, [] if unset.
     #[returns(cw721::TokensResponse)]
     Tokens {
+        /// Owner of the NFT
         owner: String,
+        /// The token id to index from
         start_after: Option<String>,
+        /// The total number of token details to return
         limit: Option<u32>,
     },
     /// Enumerable extension
     /// Requires pagination. Lists all token_ids controlled by the contract.
     #[returns(cw721::TokensResponse)]
     AllTokens {
+        /// The token id to index from
         start_after: Option<String>,
+        /// The total number of token details to return
         limit: Option<u32>,
     },
 }
