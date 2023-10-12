5b6b46d394aba6fc92a6fa37e54a5f7c3e03a46a
kwt <4344285+kwtalley@users.noreply.github.com>
2023-10-12 15:08:40 -0500

    add  validation in queries

--- contracts/nft/src/query/validate.rs
+++ contracts/nft/src/query/validate.rs
@@ -1,26 +1,31 @@
 use cosmwasm_std::{Coin, Deps};
 
 use crate::core::error::ContractError;
+use crate::util::parse_uuid;
 use crate::{core::msg::QueryMsg, util::validate::Validate};
 
 impl Validate for QueryMsg {
     fn validate(&self, _deps: Deps) -> Result<(), ContractError> {
         match self {
-            QueryMsg::AllNftInfo { .. } => Ok(()),
+            QueryMsg::AllNftInfo { token_id, .. }
+            | QueryMsg::Approvals { token_id, .. }
+            | QueryMsg::NftInfo { token_id }
+            | QueryMsg::OwnerOf { token_id, .. } => {
+                parse_uuid(token_id)?;
+            }
             QueryMsg::AllOperators { .. } => Ok(()),
             QueryMsg::AllTokens { .. } => Ok(()),
             QueryMsg::Approval { .. } => Ok(()),
-            QueryMsg::Approvals { .. } => Ok(()),
-            QueryMsg::ContractInfo { .. } => Ok(()),
-            QueryMsg::ContractVersion { .. } => Ok(()),
+            QueryMsg::ContractInfo {} => Ok(()),
+            QueryMsg::ContractVersion {} => Ok(()),
             QueryMsg::Minter { .. } => Ok(()),
-            QueryMsg::NftInfo { .. } => Ok(()),
-            QueryMsg::NumTokens { .. } => Ok(()),
+            QueryMsg::NumTokens {} => Ok(()),
             QueryMsg::Operator { .. } => Ok(()),
-            QueryMsg::OwnerOf { .. } => Ok(()),
             QueryMsg::Ownership { .. } => Ok(()),
             QueryMsg::Tokens { .. } => Ok(()),
         }
+
+        Ok(())
     }
     fn validate_funds(&self, _funds: &[Coin]) -> Result<(), ContractError> {
         Ok(())
