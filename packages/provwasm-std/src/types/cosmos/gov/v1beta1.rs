edbdcab79d4a1872e030ae9b004f914987cc23c4
kwt <4344285+kwtalley@users.noreply.github.com>
2024-01-01 11:45:51 -0500

    add gov proposal generated types

--- packages/provwasm-std/src/types/cosmos/gov/v1beta1.rs
+++ packages/provwasm-std/src/types/cosmos/gov/v1beta1.rs
@@ -54,6 +54,68 @@ pub struct Deposit {
     serde::Deserialize,
     provwasm_proc_macro::CosmwasmExt,
 )]
+#[proto_message(type_url = "/cosmos.gov.v1beta1.Proposal")]
+#[serde(rename_all = "snake_case")]
+pub struct Proposal {
+    #[prost(uint64, tag = "1")]
+    #[serde(
+        serialize_with = "crate::serde::as_str::serialize",
+        deserialize_with = "crate::serde::as_str::deserialize"
+    )]
+    pub proposal_id: u64,
+    #[prost(message, optional, tag = "2")]
+    pub content: ::core::option::Option<crate::shim::Any>,
+    #[prost(enumeration = "ProposalStatus", tag = "3")]
+    #[serde(
+        serialize_with = "ProposalStatus::serialize",
+        deserialize_with = "ProposalStatus::deserialize"
+    )]
+    pub status: i32,
+    #[prost(message, optional, tag = "4")]
+    pub final_tally_result: ::core::option::Option<TallyResult>,
+    #[prost(message, optional, tag = "5")]
+    pub submit_time: ::core::option::Option<crate::shim::Timestamp>,
+    #[prost(message, optional, tag = "6")]
+    pub deposit_end_time: ::core::option::Option<crate::shim::Timestamp>,
+    #[prost(message, repeated, tag = "7")]
+    pub total_deposit: ::prost::alloc::vec::Vec<super::super::base::v1beta1::Coin>,
+    #[prost(message, optional, tag = "8")]
+    pub voting_start_time: ::core::option::Option<crate::shim::Timestamp>,
+    #[prost(message, optional, tag = "9")]
+    pub voting_end_time: ::core::option::Option<crate::shim::Timestamp>,
+}
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(
+    Clone,
+    PartialEq,
+    ::prost::Message,
+    schemars::JsonSchema,
+    serde::Serialize,
+    serde::Deserialize,
+    provwasm_proc_macro::CosmwasmExt,
+)]
+#[proto_message(type_url = "/cosmos.gov.v1beta1.TallyResult")]
+#[serde(rename_all = "snake_case")]
+pub struct TallyResult {
+    #[prost(string, tag = "1")]
+    pub yes: ::prost::alloc::string::String,
+    #[prost(string, tag = "2")]
+    pub abstain: ::prost::alloc::string::String,
+    #[prost(string, tag = "3")]
+    pub no: ::prost::alloc::string::String,
+    #[prost(string, tag = "4")]
+    pub no_with_veto: ::prost::alloc::string::String,
+}
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(
+    Clone,
+    PartialEq,
+    ::prost::Message,
+    schemars::JsonSchema,
+    serde::Serialize,
+    serde::Deserialize,
+    provwasm_proc_macro::CosmwasmExt,
+)]
 #[proto_message(type_url = "/cosmos.gov.v1beta1.Vote")]
 #[serde(rename_all = "snake_case")]
 pub struct Vote {
@@ -199,6 +261,107 @@ impl VoteOption {
         }
     }
 }
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
+#[repr(i32)]
+#[derive(strum_macros::FromRepr, serde::Serialize, serde::Deserialize, schemars::JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum ProposalStatus {
+    Unspecified = 0,
+    DepositPeriod = 1,
+    VotingPeriod = 2,
+    Passed = 3,
+    Rejected = 4,
+    Failed = 5,
+}
+impl ProposalStatus {
+    /// String value of the enum field names used in the ProtoBuf definition.
+    ///
+    /// The values are not transformed in any way and thus are considered stable
+    /// (if the ProtoBuf definition does not change) and safe for programmatic use.
+    pub fn as_str_name(&self) -> &'static str {
+        match self {
+            ProposalStatus::Unspecified => "PROPOSAL_STATUS_UNSPECIFIED",
+            ProposalStatus::DepositPeriod => "PROPOSAL_STATUS_DEPOSIT_PERIOD",
+            ProposalStatus::VotingPeriod => "PROPOSAL_STATUS_VOTING_PERIOD",
+            ProposalStatus::Passed => "PROPOSAL_STATUS_PASSED",
+            ProposalStatus::Rejected => "PROPOSAL_STATUS_REJECTED",
+            ProposalStatus::Failed => "PROPOSAL_STATUS_FAILED",
+        }
+    }
+    /// Creates an enum from field names used in the ProtoBuf definition.
+    pub fn from_str_name(value: &str) -> ::core::option::Option<Self> {
+        match value {
+            "PROPOSAL_STATUS_UNSPECIFIED" => Some(Self::Unspecified),
+            "PROPOSAL_STATUS_DEPOSIT_PERIOD" => Some(Self::DepositPeriod),
+            "PROPOSAL_STATUS_VOTING_PERIOD" => Some(Self::VotingPeriod),
+            "PROPOSAL_STATUS_PASSED" => Some(Self::Passed),
+            "PROPOSAL_STATUS_REJECTED" => Some(Self::Rejected),
+            "PROPOSAL_STATUS_FAILED" => Some(Self::Failed),
+            _ => None,
+        }
+    }
+    pub fn serialize<S>(v: &i32, serializer: S) -> std::result::Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        let enum_value = Self::from_repr(*v);
+        match enum_value {
+            Some(v) => serializer.serialize_str(v.as_str_name()),
+            None => Err(serde::ser::Error::custom("unknown value")),
+        }
+    }
+    pub fn deserialize<'de, D>(deserializer: D) -> std::result::Result<i32, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        use serde::de::Deserialize;
+        let s = String::deserialize(deserializer)?;
+        match Self::from_str_name(&s) {
+            Some(v) => Ok(v.into()),
+            None => Err(serde::de::Error::custom("unknown value")),
+        }
+    }
+}
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(
+    Clone,
+    PartialEq,
+    ::prost::Message,
+    schemars::JsonSchema,
+    serde::Serialize,
+    serde::Deserialize,
+    provwasm_proc_macro::CosmwasmExt,
+)]
+#[proto_message(type_url = "/cosmos.gov.v1beta1.QueryProposalRequest")]
+#[serde(rename_all = "snake_case")]
+#[proto_query(
+    path = "/cosmos.gov.v1beta1.Query/Proposal",
+    response_type = QueryProposalResponse
+)]
+pub struct QueryProposalRequest {
+    #[prost(uint64, tag = "1")]
+    #[serde(
+        serialize_with = "crate::serde::as_str::serialize",
+        deserialize_with = "crate::serde::as_str::deserialize"
+    )]
+    pub proposal_id: u64,
+}
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(
+    Clone,
+    PartialEq,
+    ::prost::Message,
+    schemars::JsonSchema,
+    serde::Serialize,
+    serde::Deserialize,
+    provwasm_proc_macro::CosmwasmExt,
+)]
+#[proto_message(type_url = "/cosmos.gov.v1beta1.QueryProposalResponse")]
+#[serde(rename_all = "snake_case")]
+pub struct QueryProposalResponse {
+    #[prost(message, optional, tag = "1")]
+    pub proposal: ::core::option::Option<Proposal>,
+}
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(
     Clone,
@@ -323,12 +486,99 @@ pub struct QueryDepositResponse {
     #[prost(message, optional, tag = "1")]
     pub deposit: ::core::option::Option<Deposit>,
 }
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(
+    Clone,
+    PartialEq,
+    ::prost::Message,
+    schemars::JsonSchema,
+    serde::Serialize,
+    serde::Deserialize,
+    provwasm_proc_macro::CosmwasmExt,
+)]
+#[proto_message(type_url = "/cosmos.gov.v1beta1.MsgSubmitProposal")]
+#[serde(rename_all = "snake_case")]
+pub struct MsgSubmitProposal {
+    #[prost(message, optional, tag = "1")]
+    pub content: ::core::option::Option<crate::shim::Any>,
+    #[prost(message, repeated, tag = "2")]
+    pub initial_deposit: ::prost::alloc::vec::Vec<super::super::base::v1beta1::Coin>,
+    #[prost(string, tag = "3")]
+    pub proposer: ::prost::alloc::string::String,
+}
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(
+    Clone,
+    PartialEq,
+    ::prost::Message,
+    schemars::JsonSchema,
+    serde::Serialize,
+    serde::Deserialize,
+    provwasm_proc_macro::CosmwasmExt,
+)]
+#[proto_message(type_url = "/cosmos.gov.v1beta1.MsgSubmitProposalResponse")]
+#[serde(rename_all = "snake_case")]
+pub struct MsgSubmitProposalResponse {
+    #[prost(uint64, tag = "1")]
+    #[serde(
+        serialize_with = "crate::serde::as_str::serialize",
+        deserialize_with = "crate::serde::as_str::deserialize"
+    )]
+    pub proposal_id: u64,
+}
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(
+    Clone,
+    PartialEq,
+    ::prost::Message,
+    schemars::JsonSchema,
+    serde::Serialize,
+    serde::Deserialize,
+    provwasm_proc_macro::CosmwasmExt,
+)]
+#[proto_message(type_url = "/cosmos.gov.v1beta1.MsgVote")]
+#[serde(rename_all = "snake_case")]
+pub struct MsgVote {
+    #[prost(uint64, tag = "1")]
+    #[serde(
+        serialize_with = "crate::serde::as_str::serialize",
+        deserialize_with = "crate::serde::as_str::deserialize"
+    )]
+    pub proposal_id: u64,
+    #[prost(string, tag = "2")]
+    pub voter: ::prost::alloc::string::String,
+    #[prost(enumeration = "VoteOption", tag = "3")]
+    #[serde(
+        serialize_with = "VoteOption::serialize",
+        deserialize_with = "VoteOption::deserialize"
+    )]
+    pub option: i32,
+}
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(
+    Clone,
+    PartialEq,
+    ::prost::Message,
+    schemars::JsonSchema,
+    serde::Serialize,
+    serde::Deserialize,
+    provwasm_proc_macro::CosmwasmExt,
+)]
+#[proto_message(type_url = "/cosmos.gov.v1beta1.MsgVoteResponse")]
+#[serde(rename_all = "snake_case")]
+pub struct MsgVoteResponse {}
 pub struct GovQuerier<'a, Q: cosmwasm_std::CustomQuery> {
     querier: &'a cosmwasm_std::QuerierWrapper<'a, Q>,
 }
 impl<'a, Q: cosmwasm_std::CustomQuery> GovQuerier<'a, Q> {
     pub fn new(querier: &'a cosmwasm_std::QuerierWrapper<'a, Q>) -> Self {
         Self { querier }
+    }
+    pub fn proposal(
+        &self,
+        proposal_id: u64,
+    ) -> std::result::Result<QueryProposalResponse, cosmwasm_std::StdError> {
+        QueryProposalRequest { proposal_id }.query(self.querier)
     }
     pub fn vote(
         &self,
