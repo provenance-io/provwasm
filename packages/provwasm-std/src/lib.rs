d7cfda3fca83156f40b8031e600931af025e4a4c
kwt <4344285+kwtalley@users.noreply.github.com>
2024-02-02 16:54:08 -0600

    remove deprecated helper addr validation functions

--- packages/provwasm-std/src/lib.rs
+++ packages/provwasm-std/src/lib.rs
@@ -1,7 +1,6 @@
 /// The version (commit hash) of the Cosmos SDK used when generating this library.
 pub const PROVENANCE_VERSION: &str = include_str!("types/PROVENANCE_COMMIT");
 
-mod common;
 mod serde;
 pub mod shim;
 pub mod types;
