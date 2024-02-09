41b75ff03f4eea6dc2cad4f43523026b4dcff1d4
Matthew Witkowski <mwitkowski@provenance.io>
2024-02-09 13:04:02 -0500

    Add logic to query_tag.

--- contracts/nav/src/query/query_tag.rs
+++ contracts/nav/src/query/query_tag.rs
@@ -1,20 +1,24 @@
 use cosmwasm_std::{to_json_binary, Deps};
 
-use crate::core::{aliases::ProvQueryResponse, msg::QueryTagResponse};
+use crate::{
+    core::{aliases::ProvQueryResponse, msg::QueryTagResponse},
+    storage,
+};
 
 /// Performs the logic for the QueryTag messasge and obtains all addresses that contain the tag.
 ///
 /// # Arguments
 ///
 /// * `deps` - A non mutable version of the dependencies. The API, Querier, and storage can all be accessed from it.
-/// * `tag` - The tag to search for
+/// * `tag` - The tag to lookup addresses by.
 ///
 /// # Examples
 /// ```
 /// let res = handle(deps, tag)?;
 /// ```
 
-pub fn handle(_: Deps, _: String) -> ProvQueryResponse {
-    let res = QueryTagResponse { assets: vec![] };
+pub fn handle(deps: Deps, tag: String) -> ProvQueryResponse {
+    let assets = storage::asset::with_tag(deps.storage, &tag)?;
+    let res = QueryTagResponse { assets };
     Ok(to_json_binary(&res)?)
 }
