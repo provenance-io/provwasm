ae30c15e533b694222657848d3c44266dd21b2a4
Matthew Witkowski <mwitkowski@provenance.io>
2024-02-12 13:45:25 -0500

    Adds tests for add_tag_types.

--- contracts/nav/src/execute/add_tag_types.rs
+++ contracts/nav/src/execute/add_tag_types.rs
@@ -35,3 +35,77 @@ pub fn handle(deps: DepsMut, sender: Addr, tag_types: &[String]) -> ProvTxRespon
         .set_action(ActionType::AddTagTypes {})
         .add_event(UpdateTagTypesEvent::new().into()))
 }
+
+#[cfg(test)]
+mod tests {
+    use cosmwasm_std::{Addr, Attribute, Event};
+    use provwasm_mocks::mock_provenance_dependencies;
+
+    use crate::{
+        core::error::ContractError,
+        events::update_tag_types::UpdateTagTypesEvent,
+        execute::add_tag_types::handle,
+        storage,
+        testing::{
+            constants::{OWNER, TAG1, TAG2},
+            setup,
+        },
+        util::action::ActionType,
+    };
+
+    #[test]
+    fn test_must_be_owner() {
+        let mut deps = mock_provenance_dependencies();
+        let sender = Addr::unchecked("invalid");
+        let tag_types = vec![TAG1.to_string(), TAG2.to_string()];
+
+        setup::mock_contract(deps.as_mut());
+
+        let error = handle(deps.as_mut(), sender, &tag_types).expect_err("should return an error");
+
+        assert_eq!(
+            ContractError::Unauthorized {}.to_string(),
+            error.to_string()
+        );
+    }
+
+    #[test]
+    fn test_adds_one_tag() {
+        let mut deps = mock_provenance_dependencies();
+        let sender = Addr::unchecked(OWNER);
+        let tag_types = vec!["new tag".to_string()];
+        let expected_events: Vec<Event> = vec![UpdateTagTypesEvent::new().into()];
+        let expected_attributes: Vec<Attribute> = vec![ActionType::AddTagTypes {}.into()];
+
+        setup::mock_contract(deps.as_mut());
+        let res = handle(deps.as_mut(), sender, &tag_types).expect("should return an error");
+        let found = storage::tag::has_type(&deps.storage, "new tag");
+
+        assert_eq!(true, found);
+        assert_eq!(expected_events, res.events);
+        assert_eq!(expected_attributes, res.attributes);
+    }
+
+    #[test]
+    fn test_adds_tags_handles_duplicates() {
+        let mut deps = mock_provenance_dependencies();
+        let sender = Addr::unchecked(OWNER);
+        let tag_types = vec!["tag3".to_string(), "tag4".to_string(), "tag3".to_string()];
+        let expected_events: Vec<Event> = vec![UpdateTagTypesEvent::new().into()];
+        let expected_attributes: Vec<Attribute> = vec![ActionType::AddTagTypes {}.into()];
+
+        setup::mock_contract(deps.as_mut());
+        let res = handle(deps.as_mut(), sender, &tag_types).expect("should return an error");
+
+        for tag in tag_types {
+            let found = storage::tag::has_type(&deps.storage, &tag);
+            assert_eq!(true, found);
+        }
+        assert_eq!(expected_events, res.events);
+        assert_eq!(expected_attributes, res.attributes);
+    }
+
+    #[test]
+
+    fn test_overwrites_tags() {}
+}
