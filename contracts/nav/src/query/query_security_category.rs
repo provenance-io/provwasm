fedc7351a781d47cc0b30ba251e51c04ef25a292
Matthew Witkowski <mwitkowski@provenance.io>
2024-02-20 11:42:36 -0500

    Add tests for query_security_category.

--- contracts/nav/src/query/query_security_category.rs
+++ contracts/nav/src/query/query_security_category.rs
@@ -26,3 +26,104 @@ pub fn handle(deps: Deps, category: &str, paginate: Paginate<(String, Addr)>) ->
     let res = QuerySecurityCategoryResponse { assets };
     Ok(to_json_binary(&res)?)
 }
+
+#[cfg(test)]
+mod tests {
+    use cosmwasm_std::{from_json, Addr};
+    use provwasm_mocks::mock_provenance_dependencies;
+
+    use crate::{
+        core::msg::{Paginate, QuerySecurityCategoryResponse, Security},
+        query::query_security_category::handle,
+        storage,
+        testing::{
+            constants::{TAG1, TAG2},
+            setup,
+        },
+    };
+
+    #[test]
+    fn test_invalid_category() {
+        let mut deps = mock_provenance_dependencies();
+        setup::mock_contract(deps.as_mut());
+        let expected: Vec<(String, Addr)> = vec![];
+        let paginate = Paginate {
+            limit: None,
+            start_after: None,
+        };
+        let bin = handle(deps.as_ref(), "tag3", paginate).expect("should not return an error");
+
+        let response: QuerySecurityCategoryResponse =
+            from_json(&bin).expect("should return correct response");
+        assert_eq!(expected, response.assets);
+    }
+
+    #[test]
+    fn test_single_security_single_category() {
+        let mut deps = mock_provenance_dependencies();
+        setup::mock_contract(deps.as_mut());
+        let asset_addr = Addr::unchecked("test");
+        let expected: Vec<(String, Addr)> = vec![("".to_string(), asset_addr.clone())];
+        let paginate = Paginate {
+            limit: None,
+            start_after: None,
+        };
+        storage::asset::set_security(deps.as_mut().storage, &asset_addr, &Security::new(TAG1))
+            .expect("should not return an error");
+
+        let bin = handle(deps.as_ref(), TAG1, paginate).expect("should not return an error");
+
+        let response: QuerySecurityCategoryResponse =
+            from_json(&bin).expect("should return correct response");
+        assert_eq!(expected, response.assets);
+    }
+
+    #[test]
+    fn test_multi_security_single_category() {
+        let mut deps = mock_provenance_dependencies();
+        setup::mock_contract(deps.as_mut());
+        let asset_addr = Addr::unchecked("test");
+        let asset_addr2 = Addr::unchecked("test2");
+        let expected: Vec<(String, Addr)> = vec![
+            ("".to_string(), asset_addr.clone()),
+            ("".to_string(), asset_addr2.clone()),
+        ];
+        let paginate = Paginate {
+            limit: None,
+            start_after: None,
+        };
+        storage::asset::set_security(deps.as_mut().storage, &asset_addr, &Security::new(TAG1))
+            .expect("should not return an error");
+        storage::asset::set_security(deps.as_mut().storage, &asset_addr2, &Security::new(TAG1))
+            .expect("should not return an error");
+
+        let bin = handle(deps.as_ref(), TAG1, paginate).expect("should not return an error");
+
+        let response: QuerySecurityCategoryResponse =
+            from_json(&bin).expect("should return correct response");
+        assert_eq!(expected, response.assets);
+    }
+
+    #[test]
+    fn test_multi_security_multi_category() {
+        let mut deps = mock_provenance_dependencies();
+        setup::mock_contract(deps.as_mut());
+        let asset_addr = Addr::unchecked("test");
+        let asset_addr2 = Addr::unchecked("test2");
+        let expected: Vec<(String, Addr)> = vec![("".to_string(), asset_addr.clone())];
+        let paginate = Paginate {
+            limit: None,
+            start_after: None,
+        };
+        storage::asset::set_security(deps.as_mut().storage, &asset_addr, &Security::new(TAG1))
+            .expect("should not return an error");
+        storage::asset::set_security(deps.as_mut().storage, &asset_addr2, &Security::new(TAG2))
+            .expect("should not return an error");
+
+        let bin = handle(deps.as_ref(), TAG1, paginate).expect("should not return an error");
+
+        let response: QuerySecurityCategoryResponse =
+            from_json(&bin).expect("should return correct response");
+        assert_eq!(expected, response.assets);
+    }
+}
