aa13a6216b94496451112e1dee194e9eef8982a2
kwt <4344285+kwtalley@users.noreply.github.com>
2024-02-29 12:01:47 -0500

    set provenance to 1.18.0-rc2

--- packages/proto-build/src/main.rs
+++ packages/proto-build/src/main.rs
@@ -10,7 +10,7 @@ use proto_build::{
 };
 
 /// The provenance commit or tag to be cloned and used to build the proto files
-const PROVENANCE_REV: &str = "v1.17.1";
+const PROVENANCE_REV: &str = "v1.18.0-rc2";
 
 // All paths must end with a / and either be absolute or include a ./ to reference the current
 // working directory.
