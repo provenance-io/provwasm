6243eeb8733238b37526b38078b4411a04271048
kwt <4344285+kwtalley@users.noreply.github.com>
2024-03-05 12:39:18 -0500

    add clippy ignore too_many_args

--- packages/provwasm-std/src/lib.rs
+++ packages/provwasm-std/src/lib.rs
@@ -3,6 +3,7 @@ pub const PROVENANCE_VERSION: &str = include_str!("types/PROVENANCE_COMMIT");
 
 mod serde;
 pub mod shim;
+#[allow(clippy::too_many_arguments)]
 pub mod types;
 // Indicate that smart contracts that use this lib can only be run on the Provenance Blockchain.
 #[no_mangle]
