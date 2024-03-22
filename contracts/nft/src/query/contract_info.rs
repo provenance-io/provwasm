9fe67f2f0cf555f820806fe1450c042b93958f1c
kwt <4344285+kwtalley@users.noreply.github.com>
2024-03-21 21:51:38 -0500

    remove nft contract

--- contracts/nft/src/query/contract_info.rs
+++
@@ -1,8 +0,0 @@
-use cosmwasm_std::{to_binary, Binary, Deps};
-
-use crate::core::error::ContractError;
-use crate::storage::contract_info::CONTRACT_INFO;
-
-pub fn handle(deps: Deps) -> Result<Binary, ContractError> {
-    Ok(to_binary(&CONTRACT_INFO.load(deps.storage)?)?)
-}
