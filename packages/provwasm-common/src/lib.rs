29c7eadd38115eb66534842ad1ce90b9478bee13
kwt <4344285+kwtalley@users.noreply.github.com>
2024-05-30 09:43:40 -0500

    remove provwasm-common project and from root manifest

--- packages/provwasm-common/src/lib.rs
+++
@@ -1,8 +0,0 @@
-use cosmwasm_std::{Binary, QuerierResult};
-pub trait MockableQuerier {
-    fn register_custom_query(
-        &mut self,
-        path: String,
-        response_fn: Box<dyn Fn(&Binary) -> QuerierResult>,
-    );
-}
