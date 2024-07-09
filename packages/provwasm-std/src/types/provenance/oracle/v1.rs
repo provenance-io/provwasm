d17f98431be0362284a8d1e4f624878fbebb2c37
kwt <4344285+kwtalley@users.noreply.github.com>
2024-07-09 14:48:42 -0500

    add oracle types

---
+++ packages/provwasm-std/src/types/provenance/oracle/v1.rs
@@ -0,0 +1,170 @@
+use provwasm_proc_macro::CosmwasmExt;
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(
+    Clone,
+    PartialEq,
+    Eq,
+    ::prost::Message,
+    ::serde::Serialize,
+    ::serde::Deserialize,
+    ::schemars::JsonSchema,
+    CosmwasmExt,
+)]
+#[proto_message(type_url = "/provenance.oracle.v1.QueryOracleAddressRequest")]
+#[proto_query(
+    path = "/provenance.oracle.v1.Query/OracleAddress",
+    response_type = QueryOracleAddressResponse
+)]
+pub struct QueryOracleAddressRequest {}
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(
+    Clone,
+    PartialEq,
+    Eq,
+    ::prost::Message,
+    ::serde::Serialize,
+    ::serde::Deserialize,
+    ::schemars::JsonSchema,
+    CosmwasmExt,
+)]
+#[proto_message(type_url = "/provenance.oracle.v1.QueryOracleAddressResponse")]
+pub struct QueryOracleAddressResponse {
+    #[prost(string, tag = "1")]
+    pub address: ::prost::alloc::string::String,
+}
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(
+    Clone,
+    PartialEq,
+    Eq,
+    ::prost::Message,
+    ::serde::Serialize,
+    ::serde::Deserialize,
+    ::schemars::JsonSchema,
+    CosmwasmExt,
+)]
+#[proto_message(type_url = "/provenance.oracle.v1.QueryOracleRequest")]
+#[proto_query(
+    path = "/provenance.oracle.v1.Query/Oracle",
+    response_type = QueryOracleResponse
+)]
+pub struct QueryOracleRequest {
+    #[prost(bytes = "vec", tag = "1")]
+    #[serde(
+        serialize_with = "crate::serde::as_base64_encoded_string::serialize",
+        deserialize_with = "crate::serde::as_base64_encoded_string::deserialize"
+    )]
+    pub query: ::prost::alloc::vec::Vec<u8>,
+}
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(
+    Clone,
+    PartialEq,
+    Eq,
+    ::prost::Message,
+    ::serde::Serialize,
+    ::serde::Deserialize,
+    ::schemars::JsonSchema,
+    CosmwasmExt,
+)]
+#[proto_message(type_url = "/provenance.oracle.v1.QueryOracleResponse")]
+pub struct QueryOracleResponse {
+    #[prost(bytes = "vec", tag = "1")]
+    #[serde(
+        serialize_with = "crate::serde::as_base64_encoded_string::serialize",
+        deserialize_with = "crate::serde::as_base64_encoded_string::deserialize"
+    )]
+    pub data: ::prost::alloc::vec::Vec<u8>,
+}
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(
+    Clone,
+    PartialEq,
+    Eq,
+    ::prost::Message,
+    ::serde::Serialize,
+    ::serde::Deserialize,
+    ::schemars::JsonSchema,
+    CosmwasmExt,
+)]
+#[proto_message(type_url = "/provenance.oracle.v1.MsgSendQueryOracleRequest")]
+pub struct MsgSendQueryOracleRequest {
+    #[prost(bytes = "vec", tag = "1")]
+    #[serde(
+        serialize_with = "crate::serde::as_base64_encoded_string::serialize",
+        deserialize_with = "crate::serde::as_base64_encoded_string::deserialize"
+    )]
+    pub query: ::prost::alloc::vec::Vec<u8>,
+    #[prost(string, tag = "3")]
+    pub channel: ::prost::alloc::string::String,
+    #[prost(string, tag = "4")]
+    pub authority: ::prost::alloc::string::String,
+}
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(
+    Clone,
+    PartialEq,
+    Eq,
+    ::prost::Message,
+    ::serde::Serialize,
+    ::serde::Deserialize,
+    ::schemars::JsonSchema,
+    CosmwasmExt,
+)]
+#[proto_message(type_url = "/provenance.oracle.v1.MsgSendQueryOracleResponse")]
+pub struct MsgSendQueryOracleResponse {
+    #[prost(uint64, tag = "1")]
+    #[serde(
+        serialize_with = "crate::serde::as_str::serialize",
+        deserialize_with = "crate::serde::as_str::deserialize"
+    )]
+    pub sequence: u64,
+}
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(
+    Clone,
+    PartialEq,
+    Eq,
+    ::prost::Message,
+    ::serde::Serialize,
+    ::serde::Deserialize,
+    ::schemars::JsonSchema,
+    CosmwasmExt,
+)]
+#[proto_message(type_url = "/provenance.oracle.v1.MsgUpdateOracleRequest")]
+pub struct MsgUpdateOracleRequest {
+    #[prost(string, tag = "1")]
+    pub address: ::prost::alloc::string::String,
+    #[prost(string, tag = "2")]
+    pub authority: ::prost::alloc::string::String,
+}
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(
+    Clone,
+    PartialEq,
+    Eq,
+    ::prost::Message,
+    ::serde::Serialize,
+    ::serde::Deserialize,
+    ::schemars::JsonSchema,
+    CosmwasmExt,
+)]
+#[proto_message(type_url = "/provenance.oracle.v1.MsgUpdateOracleResponse")]
+pub struct MsgUpdateOracleResponse {}
+pub struct OracleQuerier<'a, Q: cosmwasm_std::CustomQuery> {
+    querier: &'a cosmwasm_std::QuerierWrapper<'a, Q>,
+}
+impl<'a, Q: cosmwasm_std::CustomQuery> OracleQuerier<'a, Q> {
+    pub fn new(querier: &'a cosmwasm_std::QuerierWrapper<'a, Q>) -> Self {
+        Self { querier }
+    }
+    pub fn oracle_address(&self) -> Result<QueryOracleAddressResponse, cosmwasm_std::StdError> {
+        QueryOracleAddressRequest {}.query(self.querier)
+    }
+    pub fn oracle(
+        &self,
+        query: ::prost::alloc::vec::Vec<u8>,
+    ) -> Result<QueryOracleResponse, cosmwasm_std::StdError> {
+        QueryOracleRequest { query }.query(self.querier)
+    }
+}
