00b8c1afb2b0f5e3e62de0f2e0ce25ae0aa42914
kwt <4344285+kwtalley@users.noreply.github.com>
2024-07-09 14:48:30 -0500

    add ibcratelimit types

---
+++ packages/provwasm-std/src/types/provenance/ibcratelimit/v1.rs
@@ -0,0 +1,125 @@
+use provwasm_proc_macro::CosmwasmExt;
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(
+    Clone,
+    PartialEq,
+    Eq,
+    ::prost::Message,
+    ::serde::Serialize,
+    ::serde::Deserialize,
+    ::schemars::JsonSchema,
+    CosmwasmExt,
+)]
+#[proto_message(type_url = "/provenance.ibcratelimit.v1.Params")]
+pub struct Params {
+    #[prost(string, tag = "1")]
+    pub contract_address: ::prost::alloc::string::String,
+}
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(
+    Clone,
+    PartialEq,
+    Eq,
+    ::prost::Message,
+    ::serde::Serialize,
+    ::serde::Deserialize,
+    ::schemars::JsonSchema,
+    CosmwasmExt,
+)]
+#[proto_message(type_url = "/provenance.ibcratelimit.v1.ParamsRequest")]
+#[proto_query(
+    path = "/provenance.ibcratelimit.v1.Query/Params",
+    response_type = ParamsResponse
+)]
+pub struct ParamsRequest {}
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(
+    Clone,
+    PartialEq,
+    Eq,
+    ::prost::Message,
+    ::serde::Serialize,
+    ::serde::Deserialize,
+    ::schemars::JsonSchema,
+    CosmwasmExt,
+)]
+#[proto_message(type_url = "/provenance.ibcratelimit.v1.ParamsResponse")]
+pub struct ParamsResponse {
+    #[prost(message, optional, tag = "1")]
+    pub params: ::core::option::Option<Params>,
+}
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(
+    Clone,
+    PartialEq,
+    Eq,
+    ::prost::Message,
+    ::serde::Serialize,
+    ::serde::Deserialize,
+    ::schemars::JsonSchema,
+    CosmwasmExt,
+)]
+#[proto_message(type_url = "/provenance.ibcratelimit.v1.MsgGovUpdateParamsRequest")]
+#[deprecated]
+pub struct MsgGovUpdateParamsRequest {
+    #[prost(string, tag = "1")]
+    pub authority: ::prost::alloc::string::String,
+    #[prost(message, optional, tag = "2")]
+    pub params: ::core::option::Option<Params>,
+}
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(
+    Clone,
+    PartialEq,
+    Eq,
+    ::prost::Message,
+    ::serde::Serialize,
+    ::serde::Deserialize,
+    ::schemars::JsonSchema,
+    CosmwasmExt,
+)]
+#[proto_message(type_url = "/provenance.ibcratelimit.v1.MsgGovUpdateParamsResponse")]
+#[deprecated]
+pub struct MsgGovUpdateParamsResponse {}
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(
+    Clone,
+    PartialEq,
+    Eq,
+    ::prost::Message,
+    ::serde::Serialize,
+    ::serde::Deserialize,
+    ::schemars::JsonSchema,
+    CosmwasmExt,
+)]
+#[proto_message(type_url = "/provenance.ibcratelimit.v1.MsgUpdateParamsRequest")]
+pub struct MsgUpdateParamsRequest {
+    #[prost(string, tag = "1")]
+    pub authority: ::prost::alloc::string::String,
+    #[prost(message, optional, tag = "2")]
+    pub params: ::core::option::Option<Params>,
+}
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(
+    Clone,
+    PartialEq,
+    Eq,
+    ::prost::Message,
+    ::serde::Serialize,
+    ::serde::Deserialize,
+    ::schemars::JsonSchema,
+    CosmwasmExt,
+)]
+#[proto_message(type_url = "/provenance.ibcratelimit.v1.MsgUpdateParamsResponse")]
+pub struct MsgUpdateParamsResponse {}
+pub struct IbcratelimitQuerier<'a, Q: cosmwasm_std::CustomQuery> {
+    querier: &'a cosmwasm_std::QuerierWrapper<'a, Q>,
+}
+impl<'a, Q: cosmwasm_std::CustomQuery> IbcratelimitQuerier<'a, Q> {
+    pub fn new(querier: &'a cosmwasm_std::QuerierWrapper<'a, Q>) -> Self {
+        Self { querier }
+    }
+    pub fn params(&self) -> Result<ParamsResponse, cosmwasm_std::StdError> {
+        ParamsRequest {}.query(self.querier)
+    }
+}
