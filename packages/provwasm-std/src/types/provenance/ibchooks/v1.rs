5b6ba2f6beaf2c80586b0f53d61f47930f2643b9
kwt <4344285+kwtalley@users.noreply.github.com>
2024-07-12 01:19:41 -0500

    add generated provenance types

--- packages/provwasm-std/src/types/provenance/ibchooks/v1.rs
+++ packages/provwasm-std/src/types/provenance/ibchooks/v1.rs
@@ -1,4 +1,22 @@
 use provwasm_proc_macro::CosmwasmExt;
+/// EventIBCHooksParamsUpdated defines the event emitted after updating ibchooks parameters.
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(
+    Clone,
+    PartialEq,
+    Eq,
+    ::prost::Message,
+    ::serde::Serialize,
+    ::serde::Deserialize,
+    ::schemars::JsonSchema,
+    CosmwasmExt,
+)]
+#[proto_message(type_url = "/provenance.ibchooks.v1.EventIBCHooksParamsUpdated")]
+pub struct EventIbcHooksParamsUpdated {
+    #[prost(string, repeated, tag = "1")]
+    pub allowed_async_ack_contracts: ::prost::alloc::vec::Vec<::prost::alloc::string::String>,
+}
+/// Params defines the allowed async ack contracts
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(
     Clone,
@@ -15,6 +33,24 @@ pub struct Params {
     #[prost(string, repeated, tag = "1")]
     pub allowed_async_ack_contracts: ::prost::alloc::vec::Vec<::prost::alloc::string::String>,
 }
+/// GenesisState is the IBC Hooks genesis state (params)
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(
+    Clone,
+    PartialEq,
+    Eq,
+    ::prost::Message,
+    ::serde::Serialize,
+    ::serde::Deserialize,
+    ::schemars::JsonSchema,
+    CosmwasmExt,
+)]
+#[proto_message(type_url = "/provenance.ibchooks.v1.GenesisState")]
+pub struct GenesisState {
+    #[prost(message, optional, tag = "1")]
+    pub params: ::core::option::Option<Params>,
+}
+/// QueryParamsRequest is the request type for the Query/Params RPC method.
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(
     Clone,
@@ -32,6 +68,7 @@ pub struct Params {
     response_type = QueryParamsResponse
 )]
 pub struct QueryParamsRequest {}
+/// QueryParamsResponse is the response type for the Query/Params RPC method.
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(
     Clone,
@@ -45,9 +82,11 @@ pub struct QueryParamsRequest {}
 )]
 #[proto_message(type_url = "/provenance.ibchooks.v1.QueryParamsResponse")]
 pub struct QueryParamsResponse {
+    /// params defines the parameters of the module.
     #[prost(message, optional, tag = "1")]
     pub params: ::core::option::Option<Params>,
 }
+/// MsgEmitIBCAck is the IBC Acknowledgement
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(
     Clone,
@@ -72,6 +111,7 @@ pub struct MsgEmitIbcAck {
     #[prost(string, tag = "3")]
     pub channel: ::prost::alloc::string::String,
 }
+/// MsgEmitIBCAckResponse is the IBC Acknowledgement response
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(
     Clone,
@@ -90,6 +130,7 @@ pub struct MsgEmitIbcAckResponse {
     #[prost(string, tag = "2")]
     pub ibc_ack: ::prost::alloc::string::String,
 }
+/// MsgUpdateParamsRequest is a request message for the UpdateParams endpoint.
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(
     Clone,
@@ -103,11 +144,14 @@ pub struct MsgEmitIbcAckResponse {
 )]
 #[proto_message(type_url = "/provenance.ibchooks.v1.MsgUpdateParamsRequest")]
 pub struct MsgUpdateParamsRequest {
+    /// authority should be the governance module account address.
     #[prost(string, tag = "1")]
     pub authority: ::prost::alloc::string::String,
+    /// params are the new param values to set.
     #[prost(message, optional, tag = "2")]
     pub params: ::core::option::Option<Params>,
 }
+/// MsgUpdateParamsResponse is a response message for the UpdateParams endpoint.
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(
     Clone,
--- packages/provwasm-std/src/types/provenance/oracle/v1.rs
+++ packages/provwasm-std/src/types/provenance/oracle/v1.rs
@@ -1,4 +1,95 @@
 use provwasm_proc_macro::CosmwasmExt;
+/// EventOracleQuerySuccess is an event for when the chain receives a successful response from an oracle query
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(
+    Clone,
+    PartialEq,
+    Eq,
+    ::prost::Message,
+    ::serde::Serialize,
+    ::serde::Deserialize,
+    ::schemars::JsonSchema,
+    CosmwasmExt,
+)]
+#[proto_message(type_url = "/provenance.oracle.v1.EventOracleQuerySuccess")]
+pub struct EventOracleQuerySuccess {
+    /// channel is the local channel that the oracle query response was received from
+    #[prost(string, tag = "1")]
+    pub channel: ::prost::alloc::string::String,
+    /// sequence_id is a unique identifier of the query
+    #[prost(string, tag = "2")]
+    pub sequence_id: ::prost::alloc::string::String,
+    /// result is the data received from the query
+    #[prost(string, tag = "3")]
+    pub result: ::prost::alloc::string::String,
+}
+/// EventOracleQueryError is an event for when the chain receives an error response from an oracle query
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(
+    Clone,
+    PartialEq,
+    Eq,
+    ::prost::Message,
+    ::serde::Serialize,
+    ::serde::Deserialize,
+    ::schemars::JsonSchema,
+    CosmwasmExt,
+)]
+#[proto_message(type_url = "/provenance.oracle.v1.EventOracleQueryError")]
+pub struct EventOracleQueryError {
+    /// channel is the local channel that the oracle query response was received from
+    #[prost(string, tag = "1")]
+    pub channel: ::prost::alloc::string::String,
+    /// sequence_id is a unique identifier of the query
+    #[prost(string, tag = "2")]
+    pub sequence_id: ::prost::alloc::string::String,
+    /// error is the error message received from the query
+    #[prost(string, tag = "3")]
+    pub error: ::prost::alloc::string::String,
+}
+/// EventOracleQueryTimeout is an event for when the chain receives a timeout from an oracle query
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(
+    Clone,
+    PartialEq,
+    Eq,
+    ::prost::Message,
+    ::serde::Serialize,
+    ::serde::Deserialize,
+    ::schemars::JsonSchema,
+    CosmwasmExt,
+)]
+#[proto_message(type_url = "/provenance.oracle.v1.EventOracleQueryTimeout")]
+pub struct EventOracleQueryTimeout {
+    /// channel is the local channel that the oracle timeout was received from
+    #[prost(string, tag = "1")]
+    pub channel: ::prost::alloc::string::String,
+    /// sequence_id is a unique identifier of the query
+    #[prost(string, tag = "2")]
+    pub sequence_id: ::prost::alloc::string::String,
+}
+/// GenesisState defines the oracle module's genesis state.
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(
+    Clone,
+    PartialEq,
+    Eq,
+    ::prost::Message,
+    ::serde::Serialize,
+    ::serde::Deserialize,
+    ::schemars::JsonSchema,
+    CosmwasmExt,
+)]
+#[proto_message(type_url = "/provenance.oracle.v1.GenesisState")]
+pub struct GenesisState {
+    /// The port to assign to the module
+    #[prost(string, tag = "2")]
+    pub port_id: ::prost::alloc::string::String,
+    /// The address of the oracle
+    #[prost(string, tag = "3")]
+    pub oracle: ::prost::alloc::string::String,
+}
+/// QueryOracleAddressRequest queries for the address of the oracle.
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(
     Clone,
@@ -16,6 +107,7 @@ use provwasm_proc_macro::CosmwasmExt;
     response_type = QueryOracleAddressResponse
 )]
 pub struct QueryOracleAddressRequest {}
+/// QueryOracleAddressResponse contains the address of the oracle.
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(
     Clone,
@@ -29,9 +121,11 @@ pub struct QueryOracleAddressRequest {}
 )]
 #[proto_message(type_url = "/provenance.oracle.v1.QueryOracleAddressResponse")]
 pub struct QueryOracleAddressResponse {
+    /// The address of the oracle
     #[prost(string, tag = "1")]
     pub address: ::prost::alloc::string::String,
 }
+/// QueryOracleRequest queries the module's oracle.
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(
     Clone,
@@ -49,6 +143,7 @@ pub struct QueryOracleAddressResponse {
     response_type = QueryOracleResponse
 )]
 pub struct QueryOracleRequest {
+    /// Query contains the query data passed to the oracle.
     #[prost(bytes = "vec", tag = "1")]
     #[serde(
         serialize_with = "crate::serde::as_base64_encoded_string::serialize",
@@ -56,6 +151,7 @@ pub struct QueryOracleRequest {
     )]
     pub query: ::prost::alloc::vec::Vec<u8>,
 }
+/// QueryOracleResponse contains the result of the query sent to the oracle.
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(
     Clone,
@@ -69,6 +165,7 @@ pub struct QueryOracleRequest {
 )]
 #[proto_message(type_url = "/provenance.oracle.v1.QueryOracleResponse")]
 pub struct QueryOracleResponse {
+    /// Data contains the json data returned from the oracle.
     #[prost(bytes = "vec", tag = "1")]
     #[serde(
         serialize_with = "crate::serde::as_base64_encoded_string::serialize",
@@ -76,6 +173,7 @@ pub struct QueryOracleResponse {
     )]
     pub data: ::prost::alloc::vec::Vec<u8>,
 }
+/// MsgSendQueryOracleRequest queries an oracle on another chain
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(
     Clone,
@@ -89,17 +187,21 @@ pub struct QueryOracleResponse {
 )]
 #[proto_message(type_url = "/provenance.oracle.v1.MsgSendQueryOracleRequest")]
 pub struct MsgSendQueryOracleRequest {
+    /// Query contains the query data passed to the oracle.
     #[prost(bytes = "vec", tag = "1")]
     #[serde(
         serialize_with = "crate::serde::as_base64_encoded_string::serialize",
         deserialize_with = "crate::serde::as_base64_encoded_string::deserialize"
     )]
     pub query: ::prost::alloc::vec::Vec<u8>,
+    /// Channel is the channel to the oracle.
     #[prost(string, tag = "3")]
     pub channel: ::prost::alloc::string::String,
+    /// The signing authority for the request
     #[prost(string, tag = "4")]
     pub authority: ::prost::alloc::string::String,
 }
+/// MsgSendQueryOracleResponse contains the id of the oracle query.
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(
     Clone,
@@ -113,6 +215,7 @@ pub struct MsgSendQueryOracleRequest {
 )]
 #[proto_message(type_url = "/provenance.oracle.v1.MsgSendQueryOracleResponse")]
 pub struct MsgSendQueryOracleResponse {
+    /// The sequence number that uniquely identifies the query.
     #[prost(uint64, tag = "1")]
     #[serde(
         serialize_with = "crate::serde::as_str::serialize",
@@ -120,6 +223,7 @@ pub struct MsgSendQueryOracleResponse {
     )]
     pub sequence: u64,
 }
+/// MsgUpdateOracleRequest is the request type for updating an oracle's contract address
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(
     Clone,
@@ -133,11 +237,14 @@ pub struct MsgSendQueryOracleResponse {
 )]
 #[proto_message(type_url = "/provenance.oracle.v1.MsgUpdateOracleRequest")]
 pub struct MsgUpdateOracleRequest {
+    /// The address of the oracle's contract
     #[prost(string, tag = "1")]
     pub address: ::prost::alloc::string::String,
+    /// The signing authorities for the request
     #[prost(string, tag = "2")]
     pub authority: ::prost::alloc::string::String,
 }
+/// MsgUpdateOracleResponse is the response type for updating the oracle.
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(
     Clone,
