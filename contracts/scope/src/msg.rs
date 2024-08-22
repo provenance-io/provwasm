fee88cc1867167748d18c75d594a29e1aa5dca55
kwt <4344285+kwtalley@users.noreply.github.com>
2024-08-22 11:02:29 -0500

    add new scope contract msgs for serde

--- contracts/scope/src/msg.rs
+++ contracts/scope/src/msg.rs
@@ -8,20 +8,79 @@ pub struct InitMsg {
 
 #[cw_serde]
 pub enum ExecuteMsg {
-    WriteScope { scope: Scope, signers: Vec<String> },
+    WriteScope {
+        scope: ScopeData,
+        signers: Vec<String>,
+    },
 }
 
 #[cw_serde]
 #[derive(QueryResponses)]
 pub enum QueryMsg {
-    #[returns(provwasm_std::types::provenance::metadata::v1::ContractSpecificationResponse)]
+    #[returns(ContractSpecResp)]
     GetContractSpec { id: String },
-    #[returns(provwasm_std::types::provenance::metadata::v1::ScopeResponse)]
+    #[returns(ScopeResp)]
     GetScope { id: String },
-    #[returns(provwasm_std::types::provenance::metadata::v1::SessionsResponse)]
+    #[returns(SessionsResp)]
     GetSessions { scope_id: String },
-    #[returns(Vec<provwasm_std::types::provenance::metadata::v1::RecordsResponse>)]
+    #[returns(Vec<RecordsResp>)]
     GetRecords { scope_id: String },
-    #[returns(provwasm_std::types::provenance::metadata::v1::RecordsResponse)]
+    #[returns(RecordsResp)]
     GetRecordByName { scope_id: String, name: String },
+}
+
+#[cw_serde]
+pub struct ContractSpecData {
+    pub id: Vec<u8>,
+    pub class_name: String,
+    pub description: String,
+    pub owner_addresses: Vec<String>,
+    pub party_type: String,
+}
+
+#[cw_serde]
+pub struct ContractSpecResp {
+    pub contract_spec: Option<ContractSpecData>,
+}
+
+#[cw_serde]
+pub struct ScopeData {
+    pub scope_id: String,
+    pub specification_id: String,
+    pub owners: Vec<String>,
+    pub value_owner_address: String,
+}
+
+#[cw_serde]
+pub struct ScopeResp {
+    pub scope: Option<ScopeData>,
+}
+
+#[cw_serde]
+pub struct SessionData {
+    pub session_id: String,
+    pub specification_id: String,
+    pub name: String,
+    pub data_access: Vec<String>,
+}
+
+#[cw_serde]
+pub struct SessionsResp {
+    pub session: Vec<SessionData>,
+}
+
+#[cw_serde]
+pub struct RecordsData {
+    pub name: String,
+    pub session_id: String,
+    pub process_name: Option<String>,
+    pub process_method: Option<String>,
+}
+
+#[cw_serde]
+pub struct RecordsResp {
+    pub records: Vec<RecordsData>,
+    pub inputs: Vec<String>,
+    pub output: String,
+    pub specification_id: String,
 }
