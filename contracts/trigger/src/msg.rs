b27afdde0abc71894884ad538e4e2183aa35dae7
kwt <4344285+kwtalley@users.noreply.github.com>
2024-08-26 14:48:53 -0500

    add new serde return types for trigger contract

--- contracts/trigger/src/msg.rs
+++ contracts/trigger/src/msg.rs
@@ -1,5 +1,9 @@
 use cosmwasm_schema::{cw_serde, QueryResponses};
 use cosmwasm_std::Uint64;
+use provwasm_std::shim::Any;
+use provwasm_std::types::provenance::trigger::v1::{
+    QueryTriggerByIdResponse, QueryTriggersResponse,
+};
 
 #[cw_serde]
 pub enum Event {
@@ -22,4 +26,55 @@ pub enum QueryMsg {
     #[returns(provwasm_std::types::provenance::trigger::v1::QueryTriggerByIdResponse)]
     #[returns(provwasm_std::types::provenance::trigger::v1::QueryTriggersResponse)]
     GetTrigger { id: Option<Uint64> },
+}
+
+#[cw_serde]
+pub struct TriggerData {
+    pub id: u64,
+    pub owner: String,
+    pub event: Option<Any>,
+    pub trigger: Vec<Any>,
+}
+
+#[cw_serde]
+pub struct TriggersByIdResp {
+    pub trigger: Option<TriggerData>,
+}
+
+impl From<QueryTriggerByIdResponse> for TriggersByIdResp {
+    fn from(response: QueryTriggerByIdResponse) -> Self {
+        TriggersByIdResp {
+            trigger: match response.trigger {
+                None => None,
+                Some(trigger) => Some(TriggerData {
+                    id: trigger.id,
+                    owner: trigger.owner,
+                    event: trigger.event,
+                    trigger: trigger.actions,
+                }),
+            },
+        }
+    }
+}
+
+#[cw_serde]
+pub struct TriggersResp {
+    pub triggers: Vec<TriggerData>,
+}
+
+impl From<QueryTriggersResponse> for TriggersResp {
+    fn from(response: QueryTriggersResponse) -> Self {
+        TriggersResp {
+            triggers: response
+                .triggers
+                .into_iter()
+                .map(|trigger| TriggerData {
+                    id: trigger.id,
+                    owner: trigger.owner,
+                    event: trigger.event,
+                    trigger: trigger.actions,
+                })
+                .collect(),
+        }
+    }
 }
