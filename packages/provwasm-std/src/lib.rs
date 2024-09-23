82548971dac54e67ea75672f330f6a769ce933e7
kwt <4344285+kwtalley@users.noreply.github.com>
2024-09-23 16:24:19 -0500

    add MetadataAddress util for encode/decode

--- packages/provwasm-std/src/lib.rs
+++ packages/provwasm-std/src/lib.rs
@@ -1,7 +1,7 @@
 /// The version (commit hash) of the Cosmos SDK used when generating this library.
 pub const PROVENANCE_VERSION: &str = include_str!("types/PROVENANCE_COMMIT");
 
-// mod serde;
+pub mod metadata_address;
 pub mod shim;
 #[allow(
     deprecated,
